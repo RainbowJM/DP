ba97eee7816d5352e1ebffa2c708a5798732ae37
RainbowJM <Jevona98@live.com>
2020-09-02 22:39:48 +0200

    made the domain layer of adres

--- src/ovchip/p2_p3/Adres.java
+++ src/ovchip/p2_p3/Adres.java
@@ -3,6 +3,10 @@ package ovchip.p2_p3;
 public class Adres {
     private int id;
     private String postcode;
+    private String huisnummer;
+    private String straat;
+    private String woonplaats;
+    private int reiziger_id;
 
 //    public Adres(){
 //    }
@@ -16,6 +20,22 @@ public class Adres {
         return postcode;
     }
 
+    public String getHuisnummer() {
+        return huisnummer;
+    }
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public int getReiziger_id() {
+        return reiziger_id;
+    }
+
     //setters
     public void setId(int id) {
         this.id = id;
@@ -23,6 +43,22 @@ public class Adres {
 
     public void setPostcode(String postcode) {
         this.postcode = postcode;
+    }
+
+    public void setHuisnummer(String huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+
+    public void setStraat(String straat) {
+        this.straat = straat;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public void setReiziger_id(int reiziger_id) {
+        this.reiziger_id = reiziger_id;
     }
 
     @Override
