c502eb164cd67229c5e6b934945d9cd731a92840
RainbowJM <Jevona98@live.com>
2020-09-07 09:35:21 +0200

    Ov_chipkaart created

---
+++ src/ovchip/p2_p3_p4/Ov_chipkaart.java
@@ -0,0 +1,74 @@
+package ovchip.p2_p3_p4;
+
+import java.sql.Date;
+import java.util.List;
+
+public class Ov_chipkaart {
+    private int kaart_nummer;
+    private Date geldig_tot;
+    private int klasse;
+    private int saldo;
+    private int id;
+
+    public Ov_chipkaart(int kaart_nummer, Date geldig_tot, int klasse, int saldo, int id) {
+        this.kaart_nummer = kaart_nummer;
+        this.geldig_tot = geldig_tot;
+        this.klasse = klasse;
+        this.saldo = saldo;
+        this.id = id;
+    }
+
+
+    // getters
+    public int getKaart_nummer() {
+        return kaart_nummer;
+    }
+
+    public Date getGeldig_tot() {
+        return geldig_tot;
+    }
+
+    public int getKlasse() {
+        return klasse;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //setters
+
+    public void setKaart_nummer(int kaart_nummer) {
+        this.kaart_nummer = kaart_nummer;
+    }
+
+    public void setGeldig_tot(Date geldig_tot) {
+        this.geldig_tot = geldig_tot;
+    }
+
+    public void setKlasse(int klasse) {
+        this.klasse = klasse;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Ov_chipkaart: " +
+                "kaart_nummer=" + kaart_nummer +
+                ", geldig_tot=" + geldig_tot +
+                ", klasse=" + klasse +
+                ", saldo=" + saldo +
+                ", id=" + id;
+    }
+}
