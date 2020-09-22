f2edaf156803c3f962ec3cbc3cbc6648abe3097b
RainbowJM <Jevona98@live.com>
2020-09-22 12:21:43 +0200

    deleted unnecessary things

--- src/ovchip/p2_p3_p4/dao/ReizigerDAO.java
+++ src/ovchip/p2_p3_p4/dao/ReizigerDAO.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public interface ReizigerDAO {
 
-   public boolean save(Reiziger reiziger);
+   boolean save(Reiziger reiziger);
 
-   public boolean update(Reiziger reiziger);
+   boolean update(Reiziger reiziger);
 
-   public boolean delete(Reiziger reiziger);
+   boolean delete(Reiziger reiziger);
 
-   public Reiziger findById(int id);
+   Reiziger findById(int id);
 
-   public List<Reiziger> findByGbdatum(String datum);
+   List<Reiziger> findByGbdatum(String datum);
 
-   public List<Reiziger> findAll();
+   List<Reiziger> findAll();
 }
