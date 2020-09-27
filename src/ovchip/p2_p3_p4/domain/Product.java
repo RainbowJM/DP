ad2cb38aa6c59d42104696270d512d9b06090c80
RainbowJM <Jevona98@live.com>
2020-09-27 13:05:24 +0200

    added class product

---
+++ src/ovchip/p2_p3_p4/domain/Product.java
@@ -0,0 +1,58 @@
+package ovchip.p2_p3_p4.domain;
+
+public class Product {
+    private int product_nummer;
+    private String naam;
+    private String beschrijving;
+    private int prijs;
+
+    public Product(int product_nummer, String naam, String beschrijving, int prijs) {
+        this.product_nummer = product_nummer;
+        this.naam = naam;
+        this.beschrijving = beschrijving;
+        this.prijs = prijs;
+    }
+
+    //getters
+    public int getProduct_nummer() {
+        return product_nummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+
+    public int getPrijs() {
+        return prijs;
+    }
+
+    //setters
+    public void setProduct_nummer(int product_nummer) {
+        this.product_nummer = product_nummer;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
+
+    public void setPrijs(int prijs) {
+        this.prijs = prijs;
+    }
+
+    @Override
+    public String toString() {
+        return "Product: " +
+                "product_nummer = " + product_nummer +
+                ", naam = " + naam +
+                ", beschrijving = " + beschrijving +
+                ", prijs = " + prijs ;
+    }
+}
