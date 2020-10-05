c469a30910c1885b728ef97f7c368f1da3980dad
RainbowJM <Jevona98@live.com>
2020-10-05 11:42:39 +0200

    changed the methodes

--- src/ovchip/p2_p3_p4_p5/dao/ProductDAO.java
+++ src/ovchip/p2_p3_p4_p5/dao/ProductDAO.java
@@ -1,6 +1,9 @@
 package ovchip.p2_p3_p4_p5.dao;
 
+import ovchip.p2_p3_p4_p5.domain.OvChipkaart;
 import ovchip.p2_p3_p4_p5.domain.Product;
+
+import java.util.List;
 
 public interface ProductDAO {
 
@@ -10,4 +13,7 @@ public interface ProductDAO {
 
     boolean delete(Product product);
 
+    List<Product> findByOVChipkaart(OvChipkaart ovChipkaart);
+
+    List<Product> findAll();
 }
